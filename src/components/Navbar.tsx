b3b8ea292ecaaa112368609b7ce86c9958b638e2
Gokulraj-374 <gokulrajv247@gmail.com>
2026-01-30 21:48:48 +0530

    feat(nav): add home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,6 +20,7 @@ const Navbar: React.FC = () => {
                 </a>
 
                 <ul className="nav-links">
+                    <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
